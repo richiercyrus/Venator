d907b2cf27ca1747a672784953b4c3fe966b8d87
richiercyrus <richiercyrus@users.noreply.github.com>
2019-04-02 07:48:37 -0400

    Added ASCII text

--- Venator.py
+++ Venator.py
@@ -576,6 +576,14 @@ if __name__ == '__main__':
 
   outputFile = hostname
   outputDirectory = os.getcwd()
+  print(""" 
+__     __               _
+\ \   / /__ _ __   __ _| |_ ___  _ __
+ \ \ / / _ \ '_ \ / _` | __/ _ \| '__|
+  \ V /  __/ | | | (_| | || (_) | |
+   \_/ \___|_| |_|\__,_|\__\___/|_|
+          """)
+  
 
 
   parser = argparse.ArgumentParser(description='Helpful information for running your macOS Hunting Script.')
